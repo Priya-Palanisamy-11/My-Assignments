b15a7e919a02b2ada1da2a1dbbb175e066134a65
Priya-Palanisamy-11 <pkarpagapriya68@gmail.com>
2026-09-03 14:20:31 +0530

    Add files via upload
    day 1

---
+++ day2/homeAssignmentDataTypes.js
@@ -0,0 +1,21 @@
+// dataTypes
+
+let userName="Priya";
+console.log(userName);
+console.log(typeof userName);
+
+let organization='Accenture';
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=1234567890;
+console.log(contactNumber);
+console.log(typeof contactNumber);  
+
+let knowsAutomation=true;
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright;
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright); 
